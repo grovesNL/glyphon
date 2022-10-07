00ec5e858d9222608be4af24aec743b0f9f08b46
Andrew Wheeler(Genusis) <lordsatin@hotmail.com>
2022-10-07 07:49:30 -0400

    Remove the ThisError Dependency. Create Display for GlyphonError

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,25 @@
-use thiserror::Error;
+use std::fmt::{self, Display, Formatter};
 
 pub type Result<T> = std::result::Result<T, GlyphonError>;
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum GlyphonError {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error(transparent)]
-    Surface(#[from] wgpu::SurfaceError),
-    #[error(transparent)]
-    Wgpu(#[from] wgpu::Error),
-    #[error(transparent)]
-    Device(#[from] wgpu::RequestDeviceError),
-    #[error("Render Error: Glyph no longer Exists within the Atlas.")]
     RemovedFromAtlas,
-    #[error("Render Error: Screen Resolution Changed Since Prepare was last executed.")]
     ScreenResolutionChanged,
-    #[error("Prepare Error: The Glyph Texture Atlas is full.")]
     AtlasFull,
+}
+
+impl Display for GlyphonError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            GlyphonError::RemovedFromAtlas => {
+                write!(f, "Render Error: Glyph no longer Exists within the Atlas.")
+            }
+            GlyphonError::ScreenResolutionChanged => write!(
+                f,
+                "Render Error: Screen Resolution Changed Since Prepare was last executed."
+            ),
+            GlyphonError::AtlasFull => write!(f, "Prepare Error: The Glyph Texture Atlas is full."),
+        }
+    }
 }
